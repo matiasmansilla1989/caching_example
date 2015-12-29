b7c25f43836f7031af34ebd0977812a4c4c9c44d
Demian Ardus <demian@toptierlabs.com>
2015-12-29 10:52:41 -0200

    optimized seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,21 +3,25 @@ publishers = []
   publishers << Publisher.new(name: Faker::Name.name)
 end
 
+# Create all Publishers in one INSERT 
 imported_publishers = Publisher.import publishers
 
+books = []
 imported_publishers.ids.each do |publisher_id|
-  books = []
-  10000000.times do |i|
+  10000.times do |i|
     books << Book.new(name: Faker::Name.name, description: Faker::Lorem.sentence, publisher_id: publisher_id)
   end
+end
 
-  imported_books = Book.import books
-  imported_books.ids.each do |book_id|
-    reviews = []
-    3.times do |j|
-      reviews << Review.new(content: Faker::Lorem.sentence, author: Faker::Name.name, book_id: book_id)
-    end
+# Create all Books in one INSERT 
+imported_books = Book.import books
 
-    Review.import reviews
+reviews = []
+imported_books.ids.each do |book_id|
+  3.times do |j|
+    reviews << Review.new(content: Faker::Lorem.sentence, author: Faker::Name.name, book_id: book_id)
   end
 end
+
+# Create all Reviews in one INSERT 
+Review.import reviews
