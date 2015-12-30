fc5a4ab34255e04b4b68d03c392e88ef58e59669
Demian Ardus <demian@toptierlabs.com>
2015-12-30 12:15:00 -0200

    initial cache idea

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,13 @@
 class BooksController < ApplicationController
-	def index
-		@books = Book.all
-	end
+  def index
+    @books = Book.all
+  end
+
+  def wrong_key
+    @books = Book.all
+  end
+
+  def russian_doll
+    @books = Book.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   
-  resources :items, only: [:index]
+  resources :books, only: [:index] do
+    collection do
+      get :without_relations
+      get :wrong_key
+      get :russian_doll
+    end    
+  end
 end
